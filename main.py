c547176cf44adb0c9812899f92a0e992550c3a85
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2023-07-09 17:48:40 +0800

    Use colorama for colored prints

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import re
 import time
 
+from colorama import init, Fore
 import dotenv
 import openai
 from telegram import constants, Update, Message
@@ -19,6 +20,7 @@ OPENAI_TOKEN = os.getenv("OPENAI_TOKEN")
 openai.api_key = OPENAI_TOKEN
 TARGET_USERNAMES = json.loads(os.getenv("TARGET_USERNAMES"))
 ALLOWED_CHATS = json.loads(os.getenv("ALLOWED_CHATS"))
+init(autoreset=True) # Colorama
 
 
 def log_message(chat_history: list, message_sender: str, message_timestamp: str, message_text: str, reply: Message=None):
@@ -51,7 +53,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Qualify message
     check_prompt = config.generate_check_prompt(message_text)
-    print("Requesting sentiment analysis...")
+    print(Fore.BLUE + "Requesting sentiment analysis...")
     sentiment_check = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature = 0.2,
@@ -62,20 +64,21 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         ]
     )
     sentiment_response = sentiment_check.choices[0].message.content
-    print("SENTIMENT:", sentiment_response)
+    print(Fore.BLUE + "SENTIMENT: " + sentiment_response)
     sentiment_cleaned = re.sub(r'\D', '', sentiment_response)
     sentiment_int = int(sentiment_cleaned) if sentiment_cleaned != "" else 0
-    print("SENTIMENT CLEANED:", sentiment_int)
-    if not (config.QUALIFICATION_THRESHOLD <= sentiment_int <= 9):
+    print(Fore.BLUE + "SENTIMENT CLEANED: " + str(sentiment_int))
+    if config.QUALIFICATION_THRESHOLD <= sentiment_int <= 9:
+        print(Fore.GREEN + "QUALIFIED")
+    else:
+        print(Fore.RED + "NOT QUALIFIED, SKIPPING...")
         return
-    
+
     # Generate roast reply
     thread = "\n\n".join(chat_history)
     main_prompt = config.generate_main_prompt(message_sender, thread)
     await context.bot.send_chat_action(chat_id=chat_id, action=constants.ChatAction.TYPING) # Show typing status
-    print("Requesting response...")
-    tries = 0
-    while tries < config.MAX_RETRIES:
+    print(Fore.BLUE + "Requesting response...")
         try:
             completion = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -86,33 +89,30 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
             main_response = completion.choices[0].message.content
         except openai.error.ServiceUnavailableError:
-            print("SERVICE UNAVAILABLE, RETRYING IN 10 SECONDS...")
-            tries += 1
-            time.sleep(10)
-            continue
+            print(Fore.RED + f"SERVICE UNAVAILABLE, RETRYING IN {config.RETRY_DELAY} SECONDS...")
     main_response_cleaned = re.sub(r'^["\']', '', main_response)
     main_response_cleaned = re.sub(r'["\']$', '', main_response_cleaned)
     main_response_cleaned = re.sub(r'^You: ', '', main_response_cleaned)
     main_response_cleaned = re.sub(r'^NUS Wordle Bot: ', '', main_response_cleaned)
-    print("RESPONSE:", main_response)
-    print("\033[91m" + "CLEANED RESPONSE: " + main_response_cleaned + "\033[00m")
+    print(Fore.LIGHTGREEN_EX + "RESPONSE: " + main_response)
+    print(Fore.GREEN + "CLEANED RESPONSE: " + main_response_cleaned)
     log_message(chat_history, "You", datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8))).strftime(config.DATE_FORMAT), main_response, message)
     await context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=message_id, text=main_response)
 
 
 if __name__ == '__main__':
     if not os.path.exists("chat_history.json"):
-        print("\033[91m" + "Chat history not found, creating new file..." + "\033[00m")
+        print(Fore.RED + "Chat history not found, creating new file...")
         with open("chat_history.json", "w") as file:
             json.dump([], file)
     with open("chat_history.json", "rb") as file:
         chat_history = json.load(file)
-        print("\033[92m" + "Chat history:" + "\033[00m")
+        print(Fore.BLUE + "Chat history:")
         for message in chat_history:
             print(message)
     defaults = Defaults(tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).defaults(defaults).build()
     message_handler = MessageHandler(filters.Chat(chat_id=ALLOWED_CHATS) & (filters.TEXT | filters.Sticker.ALL) & (~filters.COMMAND) & filters.UpdateType.MESSAGE, handle_message)
     application.add_handler(message_handler)
-    print("\033[92m" + "Bot started, waiting for messages..." + "\033[00m")
+    print(Fore.GREEN + "Bot started, waiting for messages...")
     asyncio.run(application.run_polling())
