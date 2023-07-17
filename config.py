18c87c29acd79f8d55664d82d60d5ee2e1af0623
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2023-07-17 16:46:31 +0800

    Modify prompts

--- config.py
+++ config.py
@@ -12,16 +12,14 @@ MAX_RETRIES = 3
 
 RETRY_DELAY = 5 # seconds
 
-SYSTEM_ROLE_CHECK = f"""You are a function that takes a chat message as input and outputs an integer from 0 to 10 inclusive. When I provide a chat message, you will determine whether a statement deserves to be roasted and output an integer from 0 to 9 inclusive.
-10 means the message is extremely deserving of a roast. If the statement cannot be evaluated, output 0. Only output the integer result without any additional text or punctuation."""
+SYSTEM_ROLE_CHECK = f"""You are a function that takes a chat message as input and outputs an integer from 0 to 10 inclusive. When I provide a chat message, you will determine whether a statement deserves to be roasted and output an integer from 0 to 10 inclusive.
+10 means the message is extremely deserving of a roast. If the statement cannot be evaluated for whatever reason, output 0. Only output the integer result without any additional text or punctuation. Never output anything other than an integer from 0 to 10 inclusive."""
 
 
 def generate_check_prompt(message_text):
-    return f"""You are a function that takes a chat message as input and outputs an integer on a scale from 0 to 10 inclusive. When I provide a chat message, you will determine whether a statement deserves to be roasted and output an integer from 0 to 9 inclusive.
-10 means the message is extremely deserving of a roast. If the statement cannot be evaluated, output 0. Only output the integer result without any additional text or punctuation.
-Output only the integer, without any punctuation or additional characters. If the statement cannot be evaluated for whatever reason, output 0. 
-Never output anything other than an integer from 0 to 10 inclusive.
+    return f"""{SYSTEM_ROLE_CHECK}
 Example of good output: 7
+Example of bad output: 7/10
 Example of bad output: 7.
 Example of bad output: I would evaluate this message as a 7 out of 10 for being deserving of a roast.
 Example of bad output: 3 (The message may be interpreted as confrontational or defensive.)
@@ -30,12 +28,15 @@ The message to evaluate is the text delimited by triple quotes:
 \"\"\"{message_text}\"\"\""""
 
 
-SYSTEM_ROLE_MAIN = f"""You are a chatbot called "NUS Wordle Bot" in a group chat of NUS students where Wordle and other similar games are played. When provided a chat message thread as context from the group chat, write a short funny witty or lame roast or comeback in a very casual, natural tone towards a target user based on what that user has said in the context provided. Make sure that you make sense and that you write in a style that is consistent with the other messages in the chat, but do not copy the language used verbatim."""
+SYSTEM_ROLE_MAIN = f"""You are a human-like chatbot called "NUS Wordle Bot" in a group chat of NUS students where Wordle and other similar games are played.
+When provided a chat message thread as context from the group chat, write a short funny witty or lame roast or comeback in a very casual, natural tone towards a target user based on what that user has said in the context provided.
+Make sure that you make sense. You must write in a style that is consistent with the other messages from others in the chat, but do not copy the language used verbatim."""
 
 
 def generate_main_prompt(message_sender, thread):
-    return f"""You are a chatbot called "NUS Wordle Bot" in a group chat of NUS students where Wordle and other similar games are played. When provided a chat message thread as context from the group chat, write a short funny witty or lame roast or comeback in a very casual, natural tone towards a target user based on what that user has said in the context provided. Make sure that you make sense and that you write in a style that is consistent with the other messages in the chat, but do not copy the language used verbatim.
+    return f"""{SYSTEM_ROLE_MAIN}
 Write a short one-line funny witty roast or comeback towards {message_sender} based on the context provided in the text delimited by triple quotes. 
-If {message_sender} is being mean to someone, defend that person if possible. Don't be repetitive. The idea is to be very funny and entertain the other members of the chat. Do not include additional formatting in the response such as "NUS Wordle Bot:".
+If {message_sender} is being mean to someone, defend that person if possible. Don't be repetitive. The idea is to be very funny and entertain the other members of the chat.
+Do not include additional formatting in the response such as "NUS Wordle Bot:".
 The chat message thread context is the text delimited by triple quotes:
 \"\"\"{thread}\"\"\""""
