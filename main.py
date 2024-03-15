7653e82905be8e0a3a2e19075f16a4a9ad95ff11
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2024-03-15 14:22:17 +0800

    Refactor message filters in handle_message function

--- main.py
+++ main.py
@@ -65,11 +65,9 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log_message(chat_history, message_sender, message_timestamp, message_text, message_reply)
 
     # Only run prompts for messages of specfic criteria
-    if message_sender_username in BLACKLIST_USERNAMES: # Blacklist filter
-        return
-    elif message_reply_sender == "NUS Wordle Bot": # Direct reply filter
+    if message_reply_sender == "NUS Wordle Bot" and message_sender_username not in BLACKLIST_USERNAMES: # Direct reply filter
         pass
-    elif "wordle bot" in message_text.lower(): # Keyphrase filter
+    elif "wordle bot" in message_text.lower() and message_sender_username not in BLACKLIST_USERNAMES: # Keyphrase filter
         pass
     elif message_sender_username in WHITELIST_USERNAMES or (config.FUZZY_USER_FILTER and random.randint(1,100) <= config.FUZZY_PROBABILITY): # Whitelist and fuzzy user filters
         # Qualify message
