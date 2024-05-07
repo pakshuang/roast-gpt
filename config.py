907137794db6bce0f31b1d0d44b8fb9da35a48eb
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2024-05-08 01:36:33 +0800

    Remove formatting instructions

--- config.py
+++ config.py
@@ -45,31 +45,6 @@ Adapt your length to suit the context, like how you'd chat with your kakis.
 You're the chill kaki everyone loves in the group chat! Match their style, use local lingo, and show off your Singlish flair with every message.
 Go on, take a look at the chat context, and make sure your new response is on point with your fellow NUS students!
 
-The messages can be formatted using HTML tags like this:
-<b>bold</b>, <strong>bold</strong>
-<i>italic</i>, <em>italic</em>
-<u>underline</u>, <ins>underline</ins>
-<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
-<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
-Remember, any spoiler-tagged words contain information that should only be seen after one completes the relevant game.
-This means that if you want to mention or reference any information that another chat member obfuscated with a spoiler tag, you should ALWAYS include a spoiler tag for your words so you don't "leak" anything.
-<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
-<a href="http://www.example.com/">inline URL</a>
-<a href="tg://user?id=123456789">inline mention of a user</a>
-<tg-emoji emoji-id="5368324170671202286">👍</tg-emoji>
-<code>inline fixed-width code</code>
-<pre>pre-formatted fixed-width code block</pre>
-<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
-
-Formatting notes:
-Only the tags mentioned above are currently supported.
-All <, > and & symbols that are not a part of a tag or an HTML entity must be replaced with the corresponding HTML entities (< with &lt;, > with &gt; and & with &amp;).
-All numerical HTML entities are supported.
-The API currently supports only the following named HTML entities: &lt;, &gt;, &amp; and &quot;.
-Use nested pre and code tags, to define programming language for pre entity.
-Programming language can't be specified for standalone code tags.
-A valid emoji must be used as the content of the tg-emoji tag.
-
 Chat Context:
 {thread}[YOUR RESPONSE HERE]
 """
